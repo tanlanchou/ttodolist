29799c3c86d704c108f6031a7767db538bc26350
tommy <126151085+tanlanchou@users.noreply.github.com>
2023-08-13 19:55:21 +0800

    update postman.json & fix bug

--- src/login.email/index.ts
+++ src/login.email/index.ts
@@ -6,6 +6,7 @@ import {
   UseGuards,
   Param,
   Req,
+  UseInterceptors,
 } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import {
@@ -23,6 +24,8 @@ import * as resultHelper from 'src/common/resultHelper';
 import { JwtCommonService } from 'src/auth/jwt.common.service';
 import { TempTokenGuard } from 'src/guard/temp.token.guard';
 import { ValidateEmailPipe } from 'src/pipe/validate.email.pipe';
+import { JwtAuthGuard } from 'src/guard/jwt.auth.guard';
+import { JwtInterceptor } from 'src/interceptors/jwt.interceptor';
 
 @Controller('login')
 export class LoginEmailController {
@@ -36,6 +39,11 @@ export class LoginEmailController {
 
   @Post('email/register')
   async register(@Body() registerDto: RegisterDto) {
+    const existUser = await this.userService.findUserByEmail(registerDto.email);
+    if (existUser !== null) {
+      return resultHelper.error(500, '邮箱已经注册过了，如果是您注册的请登录');
+    }
+
     try {
       const user = new User();
       user.email = registerDto.email;
@@ -48,9 +56,9 @@ export class LoginEmailController {
         user.name + user.createTime,
         curTimeSpan,
       );
-      const url = `http://${this.configService.get(
-        'HOST',
-      )}/login/email/confirm/${newUser.id}/${curTimeSpan}/${token}`;
+      const url = `${this.configService.get('HOST')}/login/email/confirm/${
+        newUser.id
+      }/${curTimeSpan}/${token}`;
 
       const template = MailService.getEmailTemplate(user.email, url);
       await this.mailService.sendEmail(
@@ -58,6 +66,35 @@ export class LoginEmailController {
         template.subject,
         template.content,
       );
+      return resultHelper.success();
+    } catch (error) {
+      return resultHelper.error(null, error.message, 500);
+    }
+  }
+
+  @Post('email/resend')
+  async resendEmail(@Body('email', ValidateEmailPipe) email: string) {
+    try {
+      const user = await this.userService.findUserByEmail(email);
+      if (!user) return resultHelper.RES401();
+
+      const curTimeSpan = new Date().getTime();
+      const token = this.tempTokenService.generateToken(
+        user.name + user.createTime,
+        curTimeSpan,
+      );
+
+      const url = `http://${this.configService.get(
+        'HOST',
+      )}/login/email/confirm/${user.id}/${curTimeSpan}/${token}`;
+
+      const template = MailService.getEmailTemplate(email, url); // 根据需要修改邮件模板的生成方法
+      await this.mailService.sendEmail(
+        email,
+        template.subject,
+        template.content,
+      );
+
       return resultHelper.success();
     } catch (error) {
       return resultHelper.error(null, error.message, 500);
@@ -98,6 +135,8 @@ export class LoginEmailController {
     }
   }
 
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(JwtInterceptor)
   @Post('email/change/password')
   async changePassword(
     @Body() changePasswordDto: ChangePasswordDto,
